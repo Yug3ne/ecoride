029304b9ff543a4f195c138872e7cc3552fcc32a
Yug3ne <eugenekoech9@gmail.com>
2025-09-01 11:44:24 +0300

    assets setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,34 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import {useEffect} from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded, error] = useFonts({
+    "Inter-Bold": require("../../assets/fonts/Inter-Bold.otf"),
+    "Inter-ExtraBold": require("../../assets/fonts/Inter-ExtraBold.otf"),
+    "Inter-ExtraLight": require("../../assets/fonts/Inter-ExtraLight.otf"),
+    "Inter-Light": require("../../assets/fonts/Inter-Light.otf"),
+    "Inter-Medium": require("../../assets/fonts/Inter-Medium.otf"),
+    "Inter-Regular": require("../../assets/fonts/Inter-Regular.otf"),
+    "Inter-SemiBold": require("../../assets/fonts/Inter-SemiBold.otf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+  return (
+    <Stack />
+  );
 }
-  
+
+
